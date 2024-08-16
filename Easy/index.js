b1b37ea0b8416ccc122b571b661d4ceebecf12fc
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-08-16 14:32:47 +0500

    Junior Dev_____

--- Easy/index.js
+++ Easy/index.js
@@ -86,3 +86,18 @@ sumDouble(2, 2); // => 8
 // makes10(9, 10); // =>  true
 // makes10(9, 9); // =>  false
 // makes10(1, 9); // =>  true
+/// ==========================================================================================================
+//  Loopga masala.
+// let son = 20, 30, 15
+
+// Berilgan sonlarni aylanib chiqing for loop bilan va consolga chiqaring...
+// Berilgan sonlarni aylangan vaqtda juft yoki da toqlarini consolga chiqaring..
+// Berilgan sonlarni faqat 10 ga bo'linadiganlarni consolga chiqaring..
+// Berilgan sonlarni ham 5ga ham 3ga bo'linadiganlarni consolga chiqaring..
+// Berilgan sonlarni faqat 4ga va 8ga bo'linadiganlarni consolga chiqaring..
+// Berilgan sonlar 10 ga teng bolganda ishini to'xtatsin..
+// Berilgan sonlar forda aylanganda har safar 5 ni tashlab o'tib ketsin..
+// Berilgan sonlar forda aylangan vaqtda aylanish tugaganda aylanish tugadi desin.
+// Berilgan sonlar forda aylanishni boshlaganda aylanish boshlandi desin? tugaganda esa tugadi desin?
+// Berilgan sonlar aylanganda ikki honali son bo'lsa ikki xonali son bir xonali son bo'lsa bir xonali son desin uch xonali son bo'lsa uch xonali son desin..
+//
