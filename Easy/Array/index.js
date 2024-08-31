eaa7ee2b044cf963deb676dc9b74afa7482398af
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-08-31 11:28:22 +0500

    Array << Basic >>

---
+++ Easy/Array/index.js
@@ -0,0 +1,34 @@
+/// ==========================================================================================================
+// Arrayga doir masalar.. << Basic >>
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const names = ["Toshmat", "Eshmat", "Abror", "Qulmat"];
+const furuit = ["olma", "bexi", "uzum", "nok", "olcha"];
+const country = ["Uzbekistan", "England", "USA", "Brazil"];
+
+// Berilgan array larni consolga chiqaring?
+// Berilgan array larni length ni aniqlang?
+// Berilgan array larni ichida malumot bor yoqligini tekshiring? bo'lsa true aks holda false?
+// Berilgan array larni stringa aylantiring?
+// Berilgan array larni bir biriga qoshib bering?
+// Berilgan array larni typesini array qilib chiqaring?
+// Berilgan array larni ichidagi har bir malumotga exses qiling?
+// Berilgan array larni loop bilan aylanib chiqing?
+// Berilgan array larni for of bilan aylanib chiqing natijani console.log ga chiqaring?
+// Berilgan array larni for in bilan aylanib chiqing natijani console.log ga chiqaring?
+// Berilgan array larni oxiridagi elementini olib bering?
+// Berilgan array larni ichidagi oxirgi elementini ochirib tashlang?
+// Berilgan array larni oxiriga malumot qo'shib bering? misol "hello" so'zini?
+// Berilgan array larni boshidan malumot qo'shib bering?
+// Berilgan array larni boshidan malumot o'chirib bering?
+// Berilgan array larni ichida number malumot bo'lsa number? bo'lmasa number yo'q desin?
+// Berilgan array larni concat metodini ishlatib bbir biriga qoshib natigani consolga chiqaring?
+// Berilgan array larni 1chi index dan boshlab oxirigacha kesib qaytaring?
+// Berilgan array larni 2chi index siga "salom" sozini qo'shib bering?
+// Berilgan array larni teskarisiga o'girib qaytaring?
+// Berilgan array larni while loop bilan aylanib chiqing?
+// Berilgan array larni ternary operatori bilan tekshirib malumot bo'lsa true aks holda false?
+// Berilgan nums arrayini ichidagi malumotlarni stringa ogirib qaytaring?
+// Berilgan array larni xar bir functionni parametrga berib consolga chiqaring?
+// Berilgan array larni functionni parametrga berib consolga chiqaring?
+// Berilgan array larni functionni parametrga berib funksiyani buttonga biriktirib button bosilsa consolga chiqsin?
